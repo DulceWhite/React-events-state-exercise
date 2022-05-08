86d78433caa57063a82525c5f8567f4d0f199167
DulceWhite <98054845+DulceWhite@users.noreply.github.com>
2022-05-07 19:50:39 -0500

    Bonus solution added, from published

--- src/bonus/Bonus.js
+++ src/bonus/Bonus.js
@@ -1,29 +1,52 @@
 import {useState} from 'react';
 
-function Bonus (){
+// function Bonus (){
     
-    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+//     const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     
-    const [letter, setLetter] = useState([letters[0]]);
+//     const [letter, setLetter] = useState([letters[0]]);
    
-    function iterLet (letters) {
-       for (let i=0; i<letters.length; i++){
-           return letters[i];
-         }
+//     function iterLet (letters) {
+//        for (let i=0; i<letters.length; i++){
+//            return letters[i];
+//          }
         
-   }
-   const newLetter = () => setLetter({iterLet});
+//    }
+//    const newLetter = () => setLetter({iterLet});
    
   
 
-    return (
-        <div>
-            <h1>{letter}</h1>
-            <button onClick={newLetter}>Next Letter</button>
+//     return (
+//         <div>
+//             <h1>{letter}</h1>
+//             <button onClick={newLetter}>Next Letter</button>
             
-        </div>
-    )
+//         </div>
+//     )
 
-}
+// }
 
 export default Bonus;
+
+// Solution to Bonus
+
+// 3b
+function Bonus(){
+    // 3d
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+    // 3h
+    const [letterCount, setLetterCount] = useState(0);
+  
+    // 3e
+    return(
+      <div>
+        <h1>{letters[letterCount]}</h1>
+        <button onClick={() => (letterCount === letters.length - 1) ? setLetterCount(0) : setLetterCount(letterCount + 1)}>NEXT LETTER</button>
+        {/* OR */}
+        {/* <button onClick={() => (letterCount < letters.length - 1) ? setLetterCount(nextLetter + 1) : setLetterCount(0)}>NEXT LETTER</button> */}
+        {/* OR */}
+        {/* <button onClick={() => setLetterCount(letterCount + 1)}>NEXT LETTER</button> */}
+      </div>
+    );
+  }
